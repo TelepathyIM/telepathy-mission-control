c88bf31ac3bb3ad70a53d1236da12a29911598ff
Alberto Mardegan <alberto.mardegan@nokia.com>
2009-01-13 16:08:07 +0200

    Licence change

--- src/sp_timestamp.h
+++ src/sp_timestamp.h
@@ -8,14 +8,14 @@
  *
  * Contact: Eero Tamminen <eero.tamminen@nokia.com>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 
- * version 2 as published by the Free Software Foundation. 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  * 
  * usage:
  *	#include "sp_timestamp.h"
