2c07daee0d2fbec63860bc29d28e3856e72b98d2
Simon McVittie <simon.mcvittie@collabora.co.uk>
2009-04-01 13:56:45 +0100

    _mcd_channel_get_immutable_properties: debug if returning NULL
    Ideally, this method should eventually have "does not return NULL
    unless caller is buggy" as an API guarantee. Until then, we can at
    least be noisy about it.

--- src/mcd-channel.c
+++ src/mcd-channel.c
@@ -869,11 +869,27 @@ mcd_channel_is_missed (McdChannel *channel)
 GHashTable *
 _mcd_channel_get_immutable_properties (McdChannel *channel)
 {
+    GHashTable *ret;
+
     g_return_val_if_fail (MCD_IS_CHANNEL (channel), NULL);
 
-    return channel->priv->tp_chan ?
-        tp_channel_borrow_immutable_properties (channel->priv->tp_chan) :
-            NULL;
+    if (channel->priv->tp_chan == NULL)
+    {
+        DEBUG ("Channel %p has no associated TpChannel", channel);
+        return NULL;
+    }
+
+    ret = tp_channel_borrow_immutable_properties (channel->priv->tp_chan);
+
+    if (ret == NULL)
+    {
+        DEBUG ("Channel %p TpChannel %s (%p) has no immutable properties yet",
+               channel, tp_proxy_get_object_path (channel->priv->tp_chan),
+               channel->priv->tp_chan);
+        return NULL;
+    }
+
+    return ret;
 }
 
 /*
