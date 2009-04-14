8f235ecafea591a16e9b243be9ea6fd0e84c9f6c
Alberto Mardegan <alberto.mardegan@nokia.com>
2009-04-14 16:34:33 +0300

    Turning a warning into a debug message

--- src/mcd-connection.c
+++ src/mcd-connection.c
@@ -287,7 +287,7 @@ _mcd_connection_set_presence (McdConnection * connection,
 
     if (!priv->tp_conn)
     {
-	g_warning ("%s: tp_conn is NULL!", G_STRFUNC);
+	DEBUG ("tp_conn is NULL", G_STRFUNC);
 	mcd_connection_connect (connection, NULL);
 	return;
     }
