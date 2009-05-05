839f32f28f5d2d09d56a8e32a63738e17b861622
Simon McVittie <simon.mcvittie@collabora.co.uk>
2009-05-05 18:56:01 +0100

    account-manager/reconnect.py: assert that Reconnect is a no-op when it should be

--- test/twisted/account-manager/reconnect.py
+++ test/twisted/account-manager/reconnect.py
@@ -1,7 +1,8 @@
 import dbus
 import dbus.service
 
-from servicetest import EventPattern, tp_name_prefix, tp_path_prefix
+from servicetest import EventPattern, tp_name_prefix, tp_path_prefix, \
+        call_async, sync_dbus
 from mctest import exec_test, SimulatedConnection, create_fakecm_account,\
         SimulatedChannel
 import constants as cs
@@ -15,17 +16,71 @@ def test(q, bus, mc):
         "password": "secrecy"}, signature='sv')
     (cm_name_ref, account) = create_fakecm_account(q, bus, mc, params)
 
-    # Enable the account
-    account.Set(cs.ACCOUNT, 'Enabled', True,
-            dbus_interface=cs.PROPERTIES_IFACE)
-    q.expect('dbus-signal',
-            path=account.object_path,
-            signal='AccountPropertyChanged',
-            interface=cs.ACCOUNT)
+    # Events that indicate that Reconnect might have done something
+    looks_like_reconnection = [
+            EventPattern('dbus-method-call', method='RequestConnection'),
+            EventPattern('dbus-method-call', method='Disconnect'),
+            ]
 
-    # Go online
-    requested_presence = dbus.Struct((dbus.UInt32(2L), dbus.String(u'brb'),
-                dbus.String(u'Be back soon!')))
+    q.forbid_events(looks_like_reconnection)
+
+    # While we want to be online but the account is disabled, Reconnect is a
+    # no-op. Set Enabled to False explicitly, so we're less reliant on initial
+    # state.
+
+    call_async(q, account, 'Set', cs.ACCOUNT, 'Enabled', False,
+            dbus_interface=cs.PROPERTIES_IFACE)
+    q.expect('dbus-return', method='Set')
+
+    requested_presence = dbus.Struct((dbus.UInt32(cs.PRESENCE_TYPE_AVAILABLE),
+        dbus.String(u'available'), dbus.String(u'')))
+    call_async(q, account, 'Set', cs.ACCOUNT,
+            'RequestedPresence', requested_presence,
+            dbus_interface=cs.PROPERTIES_IFACE)
+    q.expect('dbus-return', method='Set')
+
+    call_async(q, account, 'Reconnect', dbus_interface=cs.ACCOUNT)
+    q.expect('dbus-return', method='Reconnect')
+
+    sync_dbus(bus, q, account)
+
+    # While we want to be offline but the account is enabled, Reconnect is
+    # still a no-op.
+    requested_presence = dbus.Struct((dbus.UInt32(cs.PRESENCE_TYPE_OFFLINE),
+        dbus.String(u'offline'), dbus.String(u'')))
+    call_async(q, account, 'Set', cs.ACCOUNT,
+            'RequestedPresence', requested_presence,
+            dbus_interface=cs.PROPERTIES_IFACE)
+    q.expect_many(
+            EventPattern('dbus-return', method='Set'),
+            EventPattern('dbus-signal',
+                path=account.object_path,
+                signal='AccountPropertyChanged',
+                interface=cs.ACCOUNT),
+            )
+
+    # Enable the account
+    call_async(q, account, 'Set', cs.ACCOUNT, 'Enabled', True,
+            dbus_interface=cs.PROPERTIES_IFACE)
+    q.expect_many(
+            EventPattern('dbus-return', method='Set'),
+            EventPattern('dbus-signal',
+                path=account.object_path,
+                signal='AccountPropertyChanged',
+                interface=cs.ACCOUNT),
+            )
+
+    call_async(q, account, 'Reconnect', dbus_interface=cs.ACCOUNT)
+    q.expect('dbus-return', method='Reconnect')
+
+    sync_dbus(bus, q, account)
+
+    # Actually go online now
+
+    q.unforbid_events(looks_like_reconnection)
+
+    requested_presence = dbus.Struct((dbus.UInt32(cs.PRESENCE_TYPE_AVAILABLE),
+        dbus.String(u'brb'), dbus.String(u'Be back soon!')))
     account.Set(cs.ACCOUNT,
             'RequestedPresence', requested_presence,
             dbus_interface=cs.PROPERTIES_IFACE)
