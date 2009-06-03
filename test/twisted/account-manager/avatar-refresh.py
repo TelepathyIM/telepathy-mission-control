abedae9cec91be192b4ca546aa22b8969a79131a
Simon McVittie <simon.mcvittie@collabora.co.uk>
2009-06-03 11:54:28 +0100

    Add a regression test for refreshing avatars
    This simulates connection managers that need an avatar upload every time
    we connect, like telepathy-salut and telepathy-haze.

---
+++ test/twisted/account-manager/avatar-refresh.py
@@ -0,0 +1,106 @@
+"""Feature test for signing in and setting an avatar, on CMs like Salut where
+the avatar must be reset every time you sign in.
+"""
+
+import os
+
+import dbus
+import dbus.service
+
+from servicetest import EventPattern, tp_name_prefix, tp_path_prefix, \
+        call_async
+from mctest import exec_test, SimulatedConnection, create_fakecm_account
+import constants as cs
+
+cm_name_ref = dbus.service.BusName(
+        cs.tp_name_prefix + '.ConnectionManager.fakecm', bus=dbus.SessionBus())
+
+account_id = 'fakecm/fakeprotocol/jc_2edenton_40unatco_2eint'
+
+def preseed():
+
+    accounts_dir = os.environ['MC_ACCOUNT_DIR']
+
+    accounts_cfg = open(accounts_dir + '/accounts.cfg', 'w')
+    accounts_cfg.write("""# Telepathy accounts
+[%s]
+manager=fakecm
+protocol=fakeprotocol
+DisplayName=Work account
+NormalizedName=jc.denton@unatco.int
+param-account=jc.denton@unatco.int
+param-password=ionstorm
+Enabled=1
+ConnectAutomatically=1
+AutomaticPresenceType=2
+AutomaticPresenceStatus=available
+AutomaticPresenceMessage=My vision is augmented
+Nickname=JC
+AvatarMime=image/jpeg
+avatar_token=Deus Ex
+""" % account_id)
+    accounts_cfg.close()
+
+    os.makedirs(accounts_dir + '/' + account_id)
+    avatar_bin = open(accounts_dir + '/' + account_id + '/avatar.bin', 'w')
+    avatar_bin.write('Deus Ex')
+    avatar_bin.close()
+
+    account_connections_file = open(accounts_dir + '/.mc_connections', 'w')
+    account_connections_file.write("")
+    account_connections_file.close()
+
+def test(q, bus, mc):
+    expected_params = {
+            'account': 'jc.denton@unatco.int',
+            'password': 'ionstorm',
+            }
+
+    e = q.expect('dbus-method-call', method='RequestConnection',
+            args=['fakeprotocol', expected_params],
+            destination=cs.tp_name_prefix + '.ConnectionManager.fakecm',
+            path=cs.tp_path_prefix + '/ConnectionManager/fakecm',
+            interface=cs.tp_name_prefix + '.ConnectionManager',
+            handled=False)
+
+    conn = SimulatedConnection(q, bus, 'fakecm', 'fakeprotocol', '_',
+            'myself', has_avatars=True, avatars_persist=False)
+
+    q.dbus_return(e.message, conn.bus_name, conn.object_path, signature='so')
+
+    account_path = (cs.tp_path_prefix + '/Account/' + account_id)
+
+    e, _ = q.expect_many(
+            EventPattern('dbus-signal', signal='AccountPropertyChanged',
+                path=account_path, interface=cs.ACCOUNT,
+                predicate=(lambda e: e.args[0].get('ConnectionStatus') ==
+                    cs.CONN_STATUS_CONNECTING)),
+            EventPattern('dbus-method-call', method='Connect',
+                path=conn.object_path, handled=True, interface=cs.CONN),
+            )
+    assert e.args[0].get('Connection') in (conn.object_path, None)
+    assert e.args[0]['ConnectionStatus'] == cs.CONN_STATUS_CONNECTING
+    assert e.args[0]['ConnectionStatusReason'] == \
+            cs.CONN_STATUS_REASON_REQUESTED
+
+    conn.StatusChanged(cs.CONN_STATUS_CONNECTED, cs.CONN_STATUS_REASON_NONE)
+
+    _, _, e = q.expect_many(
+            EventPattern('dbus-method-call',
+                interface=cs.CONN_IFACE_AVATARS, method='GetKnownAvatarTokens',
+                args=[[conn.self_handle]],
+                handled=True),
+            EventPattern('dbus-method-call',
+                interface=cs.CONN_IFACE_AVATARS, method='SetAvatar',
+                args=['Deus Ex', 'image/jpeg'],
+                handled=True),
+            EventPattern('dbus-signal', signal='AccountPropertyChanged',
+                path=account_path, interface=cs.ACCOUNT,
+                predicate=lambda e: 'ConnectionStatus' in e.args[0]),
+            )
+
+    assert e.args[0]['ConnectionStatus'] == cs.CONN_STATUS_CONNECTED
+
+if __name__ == '__main__':
+    preseed()
+    exec_test(test, {})
