3cdf7ebbce398efc8934410a07fbe190a8b8ffc4
Simon McVittie <simon.mcvittie@collabora.co.uk>
2009-09-14 17:19:08 +0100

    _mcd_account_delete: don't assume that the account has parameters - failed accounts might not
    This fixes NULL pointer dereferences in account-manager/bad-cm.py, in
    which we create an account whose CM doesn't exist, and delete it.

--- src/mcd-account.c
+++ src/mcd-account.c
@@ -1019,7 +1019,7 @@ _mcd_account_delete (McdAccount *account,
 
         params = mcd_manager_get_parameters (priv->manager, priv->protocol_name);
 
-        for (p = params; p->name != NULL; p++)
+        for (p = params; p != NULL && p->name != NULL; p++)
         {
             if (p->flags & TP_CONN_MGR_PARAM_FLAG_SECRET
                 && mc_param_type (p) == G_TYPE_STRING)
