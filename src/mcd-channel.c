dcbdea0f4555dac463f3fc39d217a3857aaafcb9
Simon McVittie <simon.mcvittie@collabora.co.uk>
2009-12-16 19:20:42 +0000

    McdChannel: propagate our own status to our request, if any

--- src/mcd-channel.c
+++ src/mcd-channel.c
@@ -511,6 +511,34 @@ static void
 mcd_channel_status_changed (McdChannel *channel, McdChannelStatus status)
 {
     channel->priv->status = status;
+
+    if (channel->priv->request != NULL &&
+        !_mcd_request_is_complete (channel->priv->request))
+    {
+        if (status == MCD_CHANNEL_STATUS_FAILED)
+        {
+            const GError *error = mcd_channel_get_error (channel);
+
+            if (G_LIKELY (error != NULL))
+            {
+                _mcd_request_set_failure (channel->priv->request,
+                                          error->domain, error->code,
+                                          error->message);
+            }
+            else
+            {
+                g_critical ("Requested channel's status changed to FAILED "
+                            "without a proper error");
+                _mcd_request_set_failure (channel->priv->request,
+                                          TP_ERRORS, TP_ERROR_NOT_AVAILABLE,
+                                          "MC bug! FAILED but no error");
+            }
+        }
+        else if (status == MCD_CHANNEL_STATUS_DISPATCHED)
+        {
+            _mcd_request_set_success (channel->priv->request);
+        }
+    }
 }
 
 static void
