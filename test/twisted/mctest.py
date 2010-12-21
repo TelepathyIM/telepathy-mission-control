d43e3a50ace82ad1b4603f6ee08709bd53b7a507
Jonny Lamb <jonny.lamb@collabora.co.uk>
2010-12-21 15:59:30 +0000

    tests: add a test for DelayApprovers observers
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- test/twisted/mctest.py
+++ test/twisted/mctest.py
@@ -652,7 +652,7 @@ class SimulatedClient(object):
             observe=[], approve=[], handle=[],
             cap_tokens=[], bypass_approval=False, wants_recovery=False,
             request_notification=True, implement_get_interfaces=True,
-            is_handler=None, bypass_observers=False):
+            is_handler=None, bypass_observers=False, delay_approvers=False):
         self.q = q
         self.bus = bus
         self.bus_name = '.'.join([cs.tp_name_prefix, 'Client', clientname])
@@ -663,6 +663,7 @@ class SimulatedClient(object):
         self.handle = aasv(handle)
         self.bypass_approval = bool(bypass_approval)
         self.bypass_observers = bool(bypass_observers)
+        self.delay_approvers = bool(delay_approvers)
         self.wants_recovery = bool(wants_recovery)
         self.request_notification = bool(request_notification)
         self.handled_channels = dbus.Array([], signature='o')
@@ -753,6 +754,7 @@ class SimulatedClient(object):
         self.q.dbus_return(e.message, {
             'ObserverChannelFilter': self.observe,
             'Recover': self.wants_recovery,
+            'DelayApprovers': dbus.Boolean(self.delay_approvers),
             },
                 signature='a{sv}', bus=self.bus)
 
