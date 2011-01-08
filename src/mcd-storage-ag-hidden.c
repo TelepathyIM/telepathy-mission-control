744fdc63357fe815c1894a9cd5cfafe75791bfb3
Will Thompson <will.thompson@collabora.co.uk>
2011-01-08 15:22:30 +0000

    Set Hidden on accounts from hidden accounts-glib backend

--- src/mcd-storage-ag-hidden.c
+++ src/mcd-storage-ag-hidden.c
@@ -21,10 +21,23 @@
 
 #include "mcd-storage-ag-hidden.h"
 
-#include "mcd-debug.h"
+#include <telepathy-glib/util.h>
 
-G_DEFINE_TYPE (McdStorageAgHidden, mcd_storage_ag_hidden,
-    MCD_TYPE_ACCOUNT_MANAGER_SSO);
+#include "mcd-debug.h"
+/* FIXME: if we weren't in-tree, we wouldn't be able to include this header and
+ * we'd have to re-hardcode magic strings like "Hidden".
+ */
+#include "mcd-account-config.h"
+
+static void account_storage_iface_init (
+    McpAccountStorageIface *iface,
+    gpointer unused G_GNUC_UNUSED);
+
+G_DEFINE_TYPE_WITH_CODE (McdStorageAgHidden, mcd_storage_ag_hidden,
+    MCD_TYPE_ACCOUNT_MANAGER_SSO,
+    G_IMPLEMENT_INTERFACE (MCP_TYPE_ACCOUNT_STORAGE,
+        account_storage_iface_init);
+    );
 
 static void
 mcd_storage_ag_hidden_init (McdStorageAgHidden *self)
@@ -37,6 +50,39 @@ mcd_storage_ag_hidden_class_init (McdStorageAgHiddenClass *klass)
   McdAccountManagerSsoClass *super = MCD_ACCOUNT_MANAGER_SSO_CLASS (klass);
 
   super->service_type = "HiddenIM";
+}
+
+static gboolean
+_mcd_storage_ag_hidden_get (
+    const McpAccountStorage *self,
+    const McpAccountManager *am,
+    const gchar *account_suffix,
+    const gchar *key)
+{
+  /* Chain up to the real implementation, checking whether this is an account
+   * we care about in the process.
+   */
+  if (!_mcd_account_manager_sso_get (self, am, account_suffix, key))
+    return FALSE;
+
+  /* If the caller is looking for the "Hidden" key (or NULL, which means
+   * everything), let's fill it in. (Every account this plugin cares about
+   * should be hidden.)
+   */
+  if (key == NULL || !tp_strdiff (key, MC_ACCOUNTS_KEY_HIDDEN))
+    mcp_account_manager_set_value (am, account_suffix, MC_ACCOUNTS_KEY_HIDDEN,
+        "true");
+
+  return TRUE;
+}
+
+static void
+account_storage_iface_init (
+    McpAccountStorageIface *iface,
+    gpointer unused G_GNUC_UNUSED)
+{
+  mcp_account_storage_iface_implement_get (iface,
+      _mcd_storage_ag_hidden_get);
 }
 
 McdStorageAgHidden *
