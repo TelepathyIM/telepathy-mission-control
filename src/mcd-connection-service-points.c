7133b0e580815d6c25321bf213f5c61b75300e32
Jonny Lamb <jonny.lamb@collabora.co.uk>
2011-05-17 10:34:24 +0100

    service-points: don't depend on there being any interfaces
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- src/mcd-connection-service-points.c
+++ src/mcd-connection-service-points.c
@@ -130,16 +130,20 @@ service_point_interface_check (TpConnection *tp_conn,
     gpointer data,
     GObject *connection)
 {
-  const gchar *interface = interfaces[0];
+  const gchar *interface;
   gboolean found = FALSE;
   gboolean watch = GPOINTER_TO_UINT (data);
   guint i = 0;
 
-  while (!found && !tp_str_empty (interface))
+  if (interfaces == NULL)
+    return;
+
+  for (interface = interfaces[0];
+       !found && !tp_str_empty (interface);
+       interface = interfaces[++i])
     {
       if (!tp_strdiff (interface, TP_IFACE_CONNECTION_INTERFACE_SERVICE_POINT))
         found = TRUE;
-      interface = interfaces[++i];
     }
 
   if (!found)
