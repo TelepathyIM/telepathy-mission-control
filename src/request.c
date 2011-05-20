232cc604d7827c51456534976cf8c69446b3418b
Vivek Dasmohapatra <vivek@collabora.co.uk>
2011-05-20 20:33:58 +0100

    Catch the case where there's a hole in our logic and an internal request hasn't been cleaned up properly

--- src/request.c
+++ src/request.c
@@ -277,6 +277,15 @@ _mcd_request_dispose (GObject *object)
 
   DEBUG ("%p", object);
 
+  /* shouldn't ever actually get this far with a blocked account, *
+   * but we have to clear the lock if we do or we'll deadlock     */
+  if (_mcd_request_is_internal (self) && self->account != NULL)
+    {
+      const gchar *path = mcd_account_get_object_path (self->account);
+      _mcd_request_unblock_account (path);
+      g_warning ("internal request disposed without being handled or failed");
+    }
+
   tp_clear_object (&self->account);
   tp_clear_object (&self->clients);
   tp_clear_object (&self->predicted_handler);
