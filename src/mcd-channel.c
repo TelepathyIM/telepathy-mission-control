a41ad6c876407eeb47fa6ae4dc930d9fdae47c9e
Simon McVittie <simon.mcvittie@collabora.co.uk>
2012-09-14 15:12:00 +0100

    McdChannel: use the TpConnection's factory to make TpChannel objects
    Signed-off-by: Simon McVittie <simon.mcvittie@collabora.co.uk>
    Bug: https://bugs.freedesktop.org/show_bug.cgi?id=55391
    Reviewed-by: Xavier Claessens <xavier.claessens@collabora.co.uk>

--- src/mcd-channel.c
+++ src/mcd-channel.c
@@ -604,9 +604,10 @@ mcd_channel_new_from_properties (TpConnection *connection,
     TpChannel *tp_chan;
     GError *error = NULL;
 
+    tp_chan = tp_simple_client_factory_ensure_channel (
+        tp_proxy_get_factory (connection), connection, object_path,
+        properties, &error);
 
-    tp_chan = tp_channel_new_from_properties (connection, object_path,
-                                              properties, &error);
     if (G_UNLIKELY (error))
     {
         g_warning ("%s: got error: %s", G_STRFUNC, error->message);
@@ -689,8 +690,11 @@ _mcd_channel_create_proxy (McdChannel *channel, TpConnection *connection,
     GError *error = NULL;
 
     g_return_val_if_fail (MCD_IS_CHANNEL (channel), FALSE);
-    tp_chan = tp_channel_new_from_properties (connection, object_path,
-                                              properties, &error);
+
+    tp_chan = tp_simple_client_factory_ensure_channel (
+        tp_proxy_get_factory (connection), connection, object_path,
+        properties, &error);
+
     if (G_UNLIKELY (error))
     {
         g_warning ("%s: got error: %s", G_STRFUNC, error->message);
