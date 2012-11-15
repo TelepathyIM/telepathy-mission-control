52485ba01852a0fc7d46cb026adfbde16aa525d6
Cosimo Cecchi <cosimoc@gnome.org>
2012-11-15 12:04:30 -0500

    build: honor NOCONFIGURE in autogen.sh
    Like telepathy-glib does already.
    
    Bug: https://bugs.freedesktop.org/show_bug.cgi?id=57165
    Reviewed-By: Simon McVittie <simon.mcvittie@collabora.co.uk>

--- autogen.sh
+++ autogen.sh
@@ -20,16 +20,21 @@ fi
 echo 'Running autoreconf -i --force'
 autoreconf -i --force
 
-run_configure=true
-for arg in $*; do
-    case $arg in
-        --no-configure)
-            run_configure=false
-            ;;
-        *)
-            ;;
-    esac
-done
+# Honor NOCONFIGURE for compatibility with gnome-autogen.sh
+if test x"$NOCONFIGURE" = x; then
+    run_configure=true
+    for arg in $*; do
+        case $arg in
+            --no-configure)
+                run_configure=false
+                ;;
+            *)
+                ;;
+        esac
+    done
+else
+    run_configure=false
+fi
 
 default_args="--enable-gtk-doc --enable-mcd-plugins --enable-gnome-keyring=auto"
 
