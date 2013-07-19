96dd7d819c33f69303d54a320a6953e51f178156
Simon McVittie <simon.mcvittie@collabora.co.uk>
2013-07-19 18:02:29 +0100

    Include config.h in every .c file (again)
    This is potentially important if config.h includes things that affect
    compilation globally, like ENABLE_DEBUG, "#define inline __inline",
    or GLib, Telepathy, etc. versioned deprecation macros.
    
    <config.h> rather than "config.h" as recommended in the Autoconf manual.
    
    Bug: https://bugs.freedesktop.org/show_bug.cgi?id=69822
    Reviewed-by: Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>

--- tests/twisted/mcp-account-diversion.c
+++ tests/twisted/mcp-account-diversion.c
@@ -19,6 +19,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include <config.h>
 #include <mission-control-plugins/mission-control-plugins.h>
 
 #define DONT_DIVERT "fakecm/fakeprotocol/dontdivert"
--- tests/value-is-same.c
+++ tests/value-is-same.c
@@ -23,6 +23,8 @@
  *
  */
 
+#include <config.h>
+
 /* Yes, this is a hack */
 #include "mcd-account.c"
 
