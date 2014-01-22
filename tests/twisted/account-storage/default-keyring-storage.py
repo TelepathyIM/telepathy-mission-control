446f5fa537466c51246c5c5cc0c03f82cfb0b38e
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2014-01-22 13:59:57 +0100

    disable tests relying on the old account location

--- tests/twisted/account-storage/default-keyring-storage.py
+++ tests/twisted/account-storage/default-keyring-storage.py
@@ -18,6 +18,9 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
+print "FIXME: disabled while we don't have TP 1.0 accounts storage proper support"
+raise SystemExit(77)
+
 import time
 import os
 import os.path
